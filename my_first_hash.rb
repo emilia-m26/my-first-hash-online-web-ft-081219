bb2402e17efdaacdf92baa83aab51696dcbfc43d
Emilia ☆ <emilia.mena26@gmail.com>
2019-08-17 22:19:02 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -22,7 +22,7 @@ def retrieval
                   "oil paintings" => 3
                   }
     end
-    shipping_manifest["oil paintings"]
+    return shipping_manifest["oil paintings"]
 end
  
  
