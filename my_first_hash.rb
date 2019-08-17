5404ad2647e7180bbeb2892884ce497bde7d4241
Emilia ☆ <emilia.mena26@gmail.com>
2019-08-17 22:12:37 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -15,11 +15,10 @@ def retrieval
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
                   "oil paintings" => 3}
-
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
 end
-
+ 
+ # Look up the value of the "oil paintings" key in the shipping_manifest hash below
+ 
 def adding
   shipping_manifest = {
                   "whale bone corsets" => 5, 
