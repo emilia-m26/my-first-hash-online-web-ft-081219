332d0f06fd5ea400ea1c7a9cebbed8eb1a50a59b
Emilia ☆ <emilia.mena26@gmail.com>
2019-08-17 22:22:43 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -38,8 +38,8 @@ def adding
 
   # add 4 gun powder to the shipping_manifest hash below
 
+shipping_manifest ["gun powder" ] = 4
 
-
-  # return the shipping_manifest hash below
-
+  # return the shipping manifest
+  shipping_manifest
 end
