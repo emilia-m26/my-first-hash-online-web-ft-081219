aa9526e9e8ffb64215f39c19fefccc93fe2b4eae
Emilia ☆ <emilia.mena26@gmail.com>
2019-08-17 22:17:15 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -6,8 +6,11 @@ end
 
 
 def shipping_manifest
-  the_manifest={"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3"
-    }
+  
+  the_manifest = {
+  "whale bone corsets" => 5, 
+  "porcelain vases" => 2, 
+  "oil paintings" => 3}
 end
 
 # Look up the value of the "oil paintings" key in the shipping_manifest hash below
